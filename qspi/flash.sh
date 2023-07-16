76aa45bc56a93c56b957684f3a2f78358d809dfd
mitxela <mitxela@mitxela.com>
2023-07-16 01:09:59 +0100

    single script for flashing and creating disk image

--- qspi/flash.sh
+++ qspi/flash.sh
@@ -1,12 +1,20 @@
 #!/bin/bash -e
 
-dev=$(ls /dev/disk/by-id/usb-mitxela_Precision_Clock* | head -1)
-[[ -z "$dev" ]] && ( echo "No device found"; exit 1 )
+if [[ "$1" == "loop" ]]; then
+  echo "Creating disk image"
+  dev="temp.img"
+  rm -f "$dev"
 
-dev=$(readlink -f "$dev") 
-echo "Device $dev"
+  # fill with 0xFF
+  tr '\0' '\377' < /dev/zero | dd bs=4K of="$dev" count=4096
+else
+  dev=$(ls /dev/disk/by-id/usb-mitxela_Precision_Clock* | head -1)
+  [[ -z "$dev" ]] && ( echo "No device found"; exit 1 )
 
-
+  dev=$(readlink -f "$dev") 
+  echo "Device $dev"
+fi
+ 
 function sync_progress {
   sync &
   proc=$!
@@ -28,14 +36,16 @@ function cleanup {
 }
 trap cleanup EXIT
 
-
 sudo mkfs.fat -I -S 4096 "$dev"
-sudo mount "$dev" mnt/
+sudo mount "$dev" mnt/ # auto detects if loop device
+
 
 # Windows creates a folder with restore point info, just wastes space and causes
 # spurious writes. Suppress it by making a hidden file with the same name
 sudo touch '/mnt/System Volume Information'
-sudo fatattr +h '/mnt/System Volume Information' || echo "fatattr not installed"
+
+# mark it hidden (fails on loop device)
+sudo fatattr +h '/mnt/System Volume Information' || echo "fatattr failed"
 
 # each file individually is optional, warn but continue
 sudo cp config.txt mnt/ || true
@@ -44,6 +54,11 @@ sudo cp output/tzmap.bin mnt/ || true
 sudo cp output/fwt.bin mnt/ || true
 sudo cp output/fwd.bin mnt/ || true
 
-sync_progress
-ls -alh mnt/
 
+if [[ "$1" == "loop" ]]; then
+  mv "$dev" output/clock-qspi.img
+  echo "Created output/clock-qspi.img"
+else
+  sync_progress
+fi
+ls -alh mnt/
